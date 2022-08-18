68236f3f559a799d0baf5e55cff3a0f77dde2d88
Igor_U <fixtor75@gmail.com>
2022-08-18 11:34:46 +0300

    Задание 27 готово

--- Exe2/Program.cs
+++ Exe2/Program.cs
@@ -1,14 +1,24 @@
-﻿int newSumm(int number)
+﻿int numberLenght(int num) // Проверка количества символов в числах
 {
-    
-    int temp = 0;
-    int summ = 0;
-    for (int i = 0; int < )
+    int i = 0;
+    for (i = 0; num > 0; i++)
+        num = num / 10;
+    return i;
 }
 
-
+int newSumm(int num, int numberLenght)
+{
+    int temp = 0;
+    for (int i = 0; i <= numberLenght; i++)
+    {
+        temp = temp + num % 10;
+        num = num / 10;
+    }
+    return temp;
+}
 
 Console.Clear();
 Console.WriteLine("Введите число:");
 int newNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Число {newNumber}  {newDegree} = {newPow(newNumber, newDegree)}");
+Console.WriteLine($"Колличество цифр в числе {newNumber} = {numberLenght(newNumber)}");
+Console.WriteLine($"Число {newNumber} сумма цифр в числе = {newSumm(newNumber, numberLenght(newNumber))}");
