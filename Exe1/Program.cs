28f2a26a63b0d7d40dd6203e07b8fd3694600de0
Igor_U <fixtor75@gmail.com>
2022-08-18 11:04:34 +0300

    Задание 25 готово

---
+++ Exe1/Program.cs
@@ -0,0 +1,15 @@
+﻿int newPow(int number, int degree)
+{
+    int temp = number;
+    for (int i = 1; i < degree; i++)
+        number = temp * number;
+    return number;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int newNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень:");
+int newDegree = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {newNumber} в степени {newDegree} = {newPow(newNumber, newDegree)}");
