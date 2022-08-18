7c6d825b46c49a9021f49fa8e1577661757f86fc
Igor_U <fixtor75@gmail.com>
2022-08-18 11:08:54 +0300

    Задание 27  в процессе

---
+++ Exe2/Program.cs
@@ -0,0 +1,14 @@
+﻿int newSumm(int number)
+{
+    
+    int temp = 0;
+    int summ = 0;
+    for (int i = 0; int < )
+}
+
+
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int newNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {newNumber}  {newDegree} = {newPow(newNumber, newDegree)}");
